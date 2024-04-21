8cd317bc5277f9f647939f405c75a15316219622
Emma-924 <151037998+Emma-924@users.noreply.github.com>
2024-04-22 00:04:59 +0200

    Update Dataframe.h

--- Dataframe.h
+++ Dataframe.h
@@ -10,26 +10,39 @@ typedef struct {
     char titre[100];
     int taille_physique;
     int taille_logique;
-    int tab[];
+    int *tab;
 }COLUMN;
 
-
+typedef struct {
+    int nb_col;
+    int nb_row;
+    COLUMN **col_tab;
+}CDATAFRAME;
 
 
 COLUMN *create_column(char* title);
 int insert_value(COLUMN* col, int value);
 void delete_column(COLUMN **col);
 void print_col(COLUMN* col);
-void change_name_col(COLUMN* col,char* new_title);
+void change_name_col(CDATAFRAME *data,int n,char* new_title);
+
 int nb_occ(COLUMN* col,int x);
-int nb_occ_sup(COLUMN* col,int x);
-int nb_occ_inf(COLUMN* col,int x);
+int nb_occ_greater(COLUMN* col,int x);
+int nb_occ_lower(COLUMN* col,int x);
 int val_at_pos(COLUMN* col,int x);
 
-
-
-
-
-
+CDATAFRAME *create_dataframe();
+int add_column(CDATAFRAME * data, char* title);
+void print_database(CDATAFRAME * data);
+void print_name_column_database(COLUMN *t[]  ,int n);
+void delete_column_from_databse(CDATAFRAME* data, int n);
+int check_value_in_database(CDATAFRAME* data, int n);
+void change_database_value(CDATAFRAME* data, int col, int row, int val);
+int nb_occ_in_database(CDATAFRAME* data, int val);
+int nb_occ_greater_in_database(CDATAFRAME* data, int val);
+int nb_occ_lower_in_database(CDATAFRAME* data, int val);
+void delete_row(COLUMN* col,int n);
+void database_delete_row(CDATAFRAME* data, int n);
+void add_row_to_database(CDATAFRAME* data);
 
 #endif //PROJET_S2_DATAFRAME_DATAFRAME_H
